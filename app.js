b2f002393690714780cf856a8b4b9d0f95e578e8
gonzagt <gonzagt.js@gmail.com>
2024-08-13 19:24:17 -0300

    Función en JS creada y testeada OK. '<script>' movido abajo de todo para que funcione correctamente.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // Testeando que funcionen los botones.
 
 let alerta = () => alert('Funciona'); 
+
+const texto_user = document.getElementById('text');
+
+let logging = () => console.log(texto_user.value);
